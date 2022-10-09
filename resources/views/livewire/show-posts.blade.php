3f1bd90205b753fad980a0962d04af0800e5b430
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-09 10:20:08 -0500

    centralizar informacion usando div que se encuentran en la navegacion

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,7 +1,13 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
-    <h1>
-        Hola Livewire
-    </h1>
-    {{$posts}}
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <h1>
+            Hola Livewire
+        </h1>
+        {{$posts}}
+    </div>
 </div>
