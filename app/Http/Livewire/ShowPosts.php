d48a5c3295ffec98be893b1b0b3999e332631c60
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-09 18:12:52 -0500

    busquedas en mas de una columna

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -12,7 +12,10 @@ class ShowPosts extends Component
     public function render()
     {
         //$posts = Post::all();
-        $posts = Post::where('title', 'like', '%'.$this->txt_search.'%')->get();
+        //$posts = Post::where('title', 'like', '%'.$this->txt_search.'%')->get();
+        $posts = Post::where('title', 'like', '%'.$this->txt_search.'%')
+            ->orWhere('content', 'like', '%'.$this->txt_search.'%')
+            ->get();
         return view('livewire.show-posts', compact('posts'));
     }
 }
