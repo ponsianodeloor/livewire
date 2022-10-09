b7c0d3a285000977156534474c3638fd5dafe905
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-08 23:55:56 -0500

    creacion de una plantilla base para livewire y no usar app.blade que es por defecto al definir como un controlador al componente livewire

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -8,6 +8,7 @@ class ShowPosts extends Component
 {
     public function render()
     {
+        //return view('livewire.show-posts')->layout('layouts.template_liveware');
         return view('livewire.show-posts');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Livewire\ShowPosts;
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,7 +12,5 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', ShowPosts::class)->name('dashboard');
 });
