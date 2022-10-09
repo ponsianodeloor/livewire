c44004b679c5cace7780d74915370f69b1ef9e4b
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-09 10:53:55 -0500

    creacion de componentes para minizar el uso de divs en un componente livewire usando {{$slot}}

---
+++ resources/views/components/tabla.blade.php
@@ -0,0 +1,10 @@
+<!-- component -->
+<div class="overflow-x-auto">
+    <div class="min-w-screen min-h-screen bg-gray-100 flex bg-gray-100 font-sans overflow-hidden">
+        <div class="w-full lg:w-5/6">
+            <div class="bg-white shadow-md rounded my-6">
+                {{$slot}}
+            </div>
+        </div>
+    </div>
+</div>
