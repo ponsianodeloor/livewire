f470414de36e1d440c2d36db7cb799231f265346
Ponsiano De Loor <ponsianodeloor@gmail.com>
2022-10-11 16:38:53 -0500

    editar el post y eliminar la imagen para subir una nueva

--- app/Http/Livewire/Post/EditPost.php
+++ app/Http/Livewire/Post/EditPost.php
@@ -4,11 +4,16 @@ namespace App\Http\Livewire\Post;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Illuminate\Support\Facades\Storage;
 
 class EditPost extends Component
 {
+    use WithFileUploads;
+
     public $open_modal = false;
     public $post;
+    public $id_url_image, $url_image;
 
     protected $rules =[
         'post.title'=>'required',
@@ -17,6 +22,7 @@ class EditPost extends Component
 
     public function mount(Post $post){
         $this->post = $post;
+        $this->id_url_image = rand();
     }
 
     public function render()
@@ -28,9 +34,18 @@ class EditPost extends Component
 
         $this->validate();
 
+        if($this->url_image){
+            $url_image = str_replace('/storage/', '/public/',  $this->post->url_image);
+            Storage::disk('local')->delete([$url_image]);
+
+            $url_image = $this->url_image->store('public/images/posts');
+            $this->post->url_image = Storage::url($url_image);
+        }
+
         $this->post->save();
 
         $this->reset(['open_modal']);
+        $this->id_url_image = rand();
 
         $this->emitTo('show-posts','render');
         $this->emit('alert', 'Post', 'Actualizado correctamente', 'success');
--- resources/views/livewire/post/edit-post.blade.php
+++ resources/views/livewire/post/edit-post.blade.php
@@ -9,6 +9,23 @@
         <x-slot name="content">
 
             <div class="mb-4">
+                @if($url_image)
+                    <img src="{{$url_image->temporaryUrl()}}">
+                @else
+                    @if(str_contains($post->url_image, 'posts/'))
+                        <img src="{{asset($post->url_image)}}">
+                    @else
+                        <img src="{{$post->url_image}}">
+                    @endif
+                @endif
+            </div>
+
+            <div wire:loading wire:target="url_image" class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Cargando Imagen</strong>
+                <span class="block sm:inline">Espere mientras se carga la imagen.</span>
+            </div>
+
+            <div class="mb-4">
                 <x-jet-label value="Titulo del post"/>
                 <!-- se usa wire:model.refer cuando no se requiere interpolacion-->
                 <x-jet-input type="text" class="w-full mb-4" wire:model="post.title"/>
@@ -21,6 +38,13 @@
                 <x-textarea class="w-full" wire:model="post.content"></x-textarea>
                 {{$post->content}}
 
+            </div>
+
+            <div class="mb-4">
+                <input type="file" wire:model="url_image" id="{{$id_url_image}}">
+                <x-jetstream::input-error for="url_image" />
+
+                {{$post->url_image}}
             </div>
 
         </x-slot>
